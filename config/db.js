bced3389f1273deaba8e1bb91e43acc41c67ffd2
Mahayogi Lakshmipathi <mahayogi@scimata.com>
2016-04-15 19:10:30 +0530

    changed the db url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url': 'mongodb://localhost/flaunt'
-    /*'url': 'mongodb://flauntadmin:12345678@ds023000.mlab.com:23000/flaunt'*/
+    /*'url': 'mongodb://localhost/flaunt'*/
+    'url': 'mongodb://flauntadmin:12345678@ds023000.mlab.com:23000/flaunt'
 };
